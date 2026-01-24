bdbd1817bbe98c370087025f9ac897f95f0fd47a
ivan-cl0udz <ivanfilipets115@gmail.com>
2026-01-24 19:26:52 +0000

    final

---
+++ confessit/core/migrations/0009_merge_0007_confession_comments_able_0008_alter_tag_name.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_confession_comments_able'),
+        ('core', '0008_alter_tag_name'),
+    ]
+
+    operations = [
+    ]
